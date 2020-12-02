3680d85cc6e26bba64c2b3491eaa68c8de3561d3
madelinezec <mzechar@gmail.com>
2020-12-02 13:15:41 -0500

    fix ternary

--- services/deployRepoOptions/incoming_webhooks/deployRepoStart/source.js
+++ services/deployRepoOptions/incoming_webhooks/deployRepoStart/source.js
@@ -86,7 +86,7 @@ exports = async function(payload, response) {
     }
     
     aliases.forEach(function(alias, index) {
-      const primaryAlias = true ? index === 0 : false; 
+      const primaryAlias = (index === 0 ?  true : false); 
       const newPayload = context.functions.execute("createNewPayload", "productionDeploy", repoOwner, repoName, branchName, hashOption, true, alias, primaryAlias)
       context.functions.execute("addJobToQueue", newPayload, jobTitle, jobUserName, jobUserEmail); 
     })
