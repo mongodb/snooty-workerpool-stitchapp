9e5ddcbac0b8daada7abec15efab7acbc61949b3
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2021-04-05 09:26:14 -0700

    DOP-2064 Update the staging queue dashboard to pull from multiple staging collections (#8)
    * DOP-2064 Update the staging queue dashboard to pull from multiple staging collections
    
    * Added the flexibility in getJobById to take in the collection to query the jobID
    * Updated slack triggers to include the collection name in the dashboard URL
    * Updated dashboard job file to use the collname passed in the querystring
    * Updated GitHub Webhook to determine the collection based on the github user
    * Updated slack deploy webhook to determine the collection based on the slack user
    
    * DOP-2064 Update pages to refer to collection
    
    * Additional sanity check
    
    * PR Comments

--- functions/getJobById/source.js
+++ functions/getJobById/source.js
@@ -1,9 +1,6 @@
-exports = function(jobId){
+exports = function(jobId, collName){
   const db_name = context.values.get("db_name");
-  const coll_name = context.values.get("coll_name");
-  
-  var queueCollection = context.services.get("mongodb-atlas").db(db_name).collection(coll_name);
-
+  var queueCollection = context.services.get("mongodb-atlas").db(db_name).collection(collName);
   return queueCollection.findOne({_id: BSON.ObjectId(jobId)}).then(doc => {
       return doc;
   });
