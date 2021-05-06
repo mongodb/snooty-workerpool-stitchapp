e0732994a65775a1cffacf2f6df6628c83393bbf
Jordan Stapinski <jordan.stapinski@mongodb.com>
2021-05-06 11:53:12 -0400

    Remove PROD webhook, cleanup

--- services/strapi-build-webhook/incoming_webhooks/strapi-staging-webhook/source.js
+++ services/strapi-build-webhook/incoming_webhooks/strapi-staging-webhook/source.js
@@ -1,24 +1,22 @@
 // This function is the webhook's request handler.
 exports = async function (payload, response) {
-  const { email, name } = await JSON.parse(await payload.body.text());
-
   try {
     let jobTitle = "DevHub CMS Staging Build";
-    let jobUserName = name;
-    let jobUserEmail = email;
+    let jobUserName = "Jordan";
+    let jobUserEmail = "jordan.stapinski@mongodb.com";
     const newPayload = {
       jobType: "githubPush",
       source: "strapi",
       action: "push",
       repoName: "devhub-content-integration",
-      branchName: "test-strapi-webhook",
+      branchName: "strapi",
       isFork: true,
       private: true,
       isXlarge: true,
       // Can this be generalized to use the devhub-content master branch instead of my fork?
       repoOwner: "jestapinski",
       url: "https://github.com/jestapinski/devhub-content-integration.git",
-      newHead: "47796f9291f8237aaaaf2d9afcb5eec73566e156",
+      newHead: null,
     };
 
     context.functions.execute(
