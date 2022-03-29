26d7754c51b9f8e337ce629192e3bc6ba76454f0
Kumaraguru Periyasamy Kanthasamy <48932104+GuruPKK@users.noreply.github.com>
2022-03-28 22:26:28 -0700

    DOP-2772 Fix Dochub trigger

--- functions/addDocHubJobToQueue.js
+++ functions/addDocHubJobToQueue.js
@@ -1,5 +1,7 @@
-exports = function(payload){
-  
+
+
+exports = async function(payload){
+  const axios = require('axios').default;
   // console.log("source: ", payload.fullDocument.source, ", target: ", payload.fullDocument.target);
   console.log(JSON.stringify(payload));
   if (payload === undefined ||
@@ -10,25 +12,6 @@ exports = function(payload){
         return;
       }
         
-  const translatedPayload = { "isXlarge": true, "jobType": "publishDochub", "source": payload.fullDocument.name, "target": payload.fullDocument.url };
-  //, "email": "DocsPlatformTeam@mongodb.com" };
-  
-  //currently the DocsPlatformTeam@mongodb.com email is not set up to a slack channel/account - so no one on the team will get updates unforuntaely 
-  //putting maddie.zechar is temporary until we have a lead 
-  
-  
-  //6.3.2021
-  //switching from maddie.zechar@mongodb.com to cassidy.schaufele@mongodb.com
-  //intention is to fully remove email from this job, or otherwise mute notifications
-  //but routing to another user until it's determined if we can do that safely
-  //Cassidy Schaufele
-  
-  context.functions.execute("addJobToQueue", translatedPayload, "dochub admin", "Dochub Admin", "cassidy.schaufele@mongodb.com"); 
-  // payload, jobTitle, jobUserName, jobUserEmail
-  // name -- source
-  // url -- target
-  
-  // also need email
-  
-  
+  const translatedPayload = {  "source": payload.fullDocument.name, "target": payload.fullDocument.url };
+  await axios.post('https://qr4zntq72h.execute-api.us-east-2.amazonaws.com/prd/webhook/dochub/trigger/upsert', translatedPayload);
 };
